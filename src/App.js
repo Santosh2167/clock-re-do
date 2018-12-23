6d78584d2a175bdb6bec0ea3c32a8186c0e31cb0
Santosh <santosh.poudyal@gmail.com>
2018-12-23 18:05:43 +1100

    created constructor and moved window.navigator.... function into constructor, initialized state

--- src/App.js
+++ src/App.js
@@ -7,13 +7,37 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {location: null};
-    }
 
-    render() {
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({latitude: position.coords.latitude}),
             error => console.log(error)
         );
+    }
+
+    isWarm() {
+        const {latitude}= this.state;
+        const date = new Date();
+        const month = date.getMonth();
+
+        if(latitude){
+            if (month>=4 && month<=9 && latitude > 0){
+                console.log("it is above equator so hot here");
+            } else if (month<=3 && month>=10 && latitude<0){
+                console.log("it is below equator and it is winter here");
+            } else {
+                console.log("exactly at equator");
+            }
+        } else {
+            console.log("latitude is not available")
+        }
+
+        
+
+    }
+
+    render() {
+
+        this.isWarm();
 
         const {latitude} = this.state;
     
